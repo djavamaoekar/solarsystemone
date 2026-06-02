b52d7ea7f1aa3787645bddb6e3d3e2ffd50d41a8
javamaukar <javamaukar@gmail.com>
2026-06-02 20:44:37 +0700

    new

--- Backend/server.js
+++ Backend/server.js
@@ -146,6 +146,6 @@ app.put("/comment/:id", (req, res) => {
 
 // ===== START SERVER =====
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0' , () => {
   console.log(`Server jalan di port ${PORT}`);
 });
